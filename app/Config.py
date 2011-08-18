c8fecc6c390f22579802c1d2d17b0d9ee4afc718
Davy RENAUD <davy.renaud@laposte.net>
2011-08-18 19:36:13 +0200

    Clean the licence and delete all tabs: that 's why there so many changes

--- app/Config.py
+++ app/Config.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 #
-# ----------------------------------------------------------------------------
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <davy.renaud@laposte.net> wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return Davy Renaud (glyptostroboides)
-# ----------------------------------------------------------------------------
+# This file is part of bumblebee-ui.
 #
-
-#    This file is part of bumblebee-ui.
+# bumblebee-ui is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    bumblebee-ui is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# bumblebee-ui is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#    bumblebee-ui is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with bumblebee-ui.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with bumblebee-ui. If not, see <http://www.gnu.org/licenses/>.
 #
 ### END LICENSE
 
@@ -45,8 +37,8 @@ compression_list=['jpeg','proxy','rgb','yuv','xv']
 #MODE LIST
 #TODO Need to be set to a list maybe
 mode_keys={'perf':"Performance",
-	'eco':"Power Save",
-	'option':"Optional"}
+    'eco':"Power Save",
+    'option':"Optional"}
 
 #ICON FILES THEME
 icon_size=24
@@ -61,11 +53,11 @@ to_unconfigure_color='#FF0033'
 config_file_path='/etc/bumblebee/bumblebee.conf'
 #GET BUMBLEBEE CONFIGURATION VALUE
 def get_config_value(variable_name):
-	"""Function to get configuration value inside a shell script"""
-	for line in open(config_file_path):
-		if variable_name in line:	
-			variable, value= line.split('=',1)
-	return value.replace("\n","")
+    """Function to get configuration value inside a shell script"""
+    for line in open(config_file_path):
+        if variable_name in line:	
+            variable, value= line.split('=',1)
+    return value.replace("\n","")
 
 default_compression= get_config_value('VGL_COMPRESS')
 vgl_display= get_config_value('VGL_DISPLAY').replace(":","")
@@ -74,24 +66,24 @@ vgl_display= get_config_value('VGL_DISPLAY').replace(":","")
 
 ####TODO There might be a way to get those key from a menu configuration file
 categorie_list=[['Game',	'applications-games'],
-		['AudioVideo',	'applications-multimedia'],
-		['Graphics',	'applications-graphics'],
-		['Network',	'applications-internet'],
-		['Office',	'applications-office'],
-		['Settings',	'applications-system'],
-		['System',	'applications-electronics'],
-		['Utility',	'applications-utilities']]
+        ['AudioVideo',	'applications-multimedia'],
+        ['Graphics',	'applications-graphics'],
+        ['Network',	'applications-internet'],
+        ['Office',	'applications-office'],
+        ['Settings',	'applications-system'],
+        ['System',	'applications-electronics'],
+        ['Utility',	'applications-utilities']]
 unmatch_categorie=['Miscellaneous','applications-other']
 uncategorized_categorie=['Uncategorized', 'application-x-executable']
 
 #DEFAULT APPLICATIONS IN THE PREFERRED APP MENU :
 default_preferred_apps =[ ['Glxgears', ['optirun', 'glxgears']] , 
-			  ['Glxspheres', ['optirun', 'glxspheres']] ]
+                        ['Glxspheres', ['optirun', 'glxspheres']] ]
 
 #TODO : There might be a way to use string formatting to simplify the config definition
 #FIXME There must be a better way to store config
 
 if __name__=="__main__" : 
-	print "Config.py can't run as a standalone application"
-	quit()
+    print "Config.py can't run as a standalone application"
+    quit()
 
